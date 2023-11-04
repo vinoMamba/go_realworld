da4e7763dae9d9b45f6cde2d3d3e2822636e182e
wangxin <vino0908@outlook.com>
2023-11-04 17:23:16 +0800

    feat: verify token

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -9,7 +10,7 @@ import (
 
 func GenerateJWT(email, username string) (string, error) {
 	iat := time.Now()
-	exp := iat.Add(24 * time.Hour)
+	exp := iat.Add(24 * time.Second)
 
 	jwtKey := []byte(config.GetJWTSecret())
 	t := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
@@ -21,4 +22,20 @@ func GenerateJWT(email, username string) (string, error) {
 		"exp": exp.Unix(),
 	})
 	return t.SignedString(jwtKey)
+}
+
+func VerifyJWT(token string) (*jwt.MapClaims, bool, error) {
+	var claim jwt.MapClaims
+	t, err := jwt.ParseWithClaims(token, &claim, func(token *jwt.Token) (interface{}, error) {
+		return []byte(config.GetJWTSecret()), nil
+	})
+	if err != nil {
+		return nil, false, err
+	}
+	if t.Valid {
+		fmt.Printf("claim:%v\n", claim)
+		return nil, true, nil
+	} else {
+		return &claim, false, nil
+	}
 }
--- utils/jwt_test.go
+++ utils/jwt_test.go
@@ -12,3 +12,22 @@ func TestGenerateJWT(t *testing.T) {
 	}
 	t.Logf("token:%v\n", token)
 }
+
+func TestVerifyJWT(t *testing.T) {
+	token, err := GenerateJWT("vino@test.com", "vino")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	claim, ok, err := VerifyJWT(token)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if !ok {
+		t.Error("verify failed")
+		return
+	}
+	t.Logf("claim: %v\n", JsonMarshal(claim))
+}
